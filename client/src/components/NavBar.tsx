e15885e02c143cce2d5377ca70b78414b980338d
DenisSavostenko0858 <denyotube0858@gmail.com>
2024-07-14 20:04:35 +0300

    added a navbar for client and install vite

---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,28 @@
+function NavBar(){
+    return(
+        <div className="container-nav-bar">
+            <div className="nav-bar-left">
+                <div className="nav-bar-logo">
+                    <h2>Kinopolis</h2>
+                </div>
+                <div className="nav-bar-links">
+                    <a href="">Главная</a>
+                    <a href="">Фильмы</a>
+                    <a href="">Категории</a>
+                    <a href="">О проекте</a>
+                </div>
+            </div>
+            <div className="nav-bar-right">
+                <div className="nav-bar-search">
+                    <input type="text" placeholder="Поиск..." className="input-search"/>
+                </div>
+                <div className="nav-bar-auth">
+                    <a href="">Войти</a>
+                    <a href="">Регистрация</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
