d02208ca1a24a59a693d6bc07a1fe1de1acfb50d
DenisSavostenko0858 <denyotube0858@gmail.com>
2024-07-22 16:58:57 +0300

    add functiom create brand and create type

--- client/src/http/movieAPI.tsx
+++ client/src/http/movieAPI.tsx
@@ -4,3 +4,11 @@ export const addmovies = async (name:string, rating:number, image:string, descri
     const { data } = await $host.post('api/admin/addmovies', {name, rating, image, description, typeId, brandId});
     return data
 };
+export const addtype = async (name:string): Promise<any> => {
+    const { data } = await $host.post('api/type/addtype', {name});
+    return data
+};
+export const addbrand = async (name:string): Promise<any> => {
+    const { data } = await $host.post('api/brand/addbrand', {name});
+    return data
+};
--- client/src/pages/AdminPanel.tsx
+++ client/src/pages/AdminPanel.tsx
@@ -1,7 +1,7 @@
 import { observer } from 'mobx-react-lite';
 import { Context } from '../main';
 import { useState, useContext } from 'react';
-import { addmovies } from '../http/movieAPI';
+import { addmovies, addtype, addbrand } from '../http/movieAPI';
 import { ADMIN_ROUT } from '../utils/consts_rout';
 import { useNavigate } from 'react-router-dom';
 
@@ -16,7 +16,7 @@ const Admin = observer(() => {
     const [typeId, setTypeId] = useState('');
     const [brandId, setBrandId] = useState('');
 
-    const click = async (): Promise<void> => {
+    const addMovie = async (): Promise<void> => {
         try {
             let data;
             data = await addmovies(name, parseFloat(rating), image, description, parseFloat(typeId), parseFloat(brandId));
@@ -28,54 +28,126 @@ const Admin = observer(() => {
             console.log(error.message); 
         }
       }
+    
+    const [typeName, setTypeName] = useState('');
+    
+    const addType = async (): Promise<void> => {
+        try {
+            let data;
+            data = await addtype(typeName);
+            
+            context?.user.setUser(data);
+            context?.user.setIsAuth(true);
+            history(ADMIN_ROUT);
+        } catch (error: any) {
+            console.log(error.message); 
+        }     
+    } 
+
+    const [brandName, setBrandName] = useState('');
+    
+    const addBrand = async (): Promise<void> => {
+        try {
+            let data;
+            data = await addbrand(brandName);
+            
+            context?.user.setUser(data);
+            context?.user.setIsAuth(true);
+            history(ADMIN_ROUT);
+        } catch (error: any) {
+            console.log(error.message); 
+        }     
+    } 
     return(
         <div className='container'>
             <h2>Админ панель</h2>
-            <h3>Добавить фильм</h3>
-            <div className='container-admin-panel'>
-                <div className='form-add-movie'>
-                    <input
-                            className="input-add-movie"
-                            placeholder="Название..."
-                            value={name}
-                            onChange={e => setName(e.target.value)}
-                    />
-                    <input
-                            className="input-add-movie"
-                            placeholder="Рейтинг"
-                            value={rating}
-                            onChange={e => setRating(e.target.value)}
-                    />
-                    <input
-                            className="input-add-movie"
-                            placeholder="Путь к изображению.."
-                            value={image}
-                            onChange={e => setImage(e.target.value)}
-                    />
-                    <input
-                            className="input-add-movie"
-                            placeholder="Описание..."
-                            value={description}
-                            onChange={e => setDescription(e.target.value)}
-                    />
-                    <input
-                            className="input-add-movie"
-                            placeholder="Жанр..."
-                            value={typeId}
-                            onChange={e => setTypeId(e.target.value)}
-                    />
-                    <input
-                            className="input-add-movie"
-                            placeholder="Студия..."
-                            value={brandId}
-                            onChange={e => setBrandId(e.target.value)}
-                    />
-                     <button
-                                className='btn-admin-panel'
-                                onClick={click}
-                            >
-                                {'Добавить фильм'}
-                    </button>
+            <div className="admin-panel-content">
+                <div style={{display:"flex", flexDirection:"column", marginBottom:"20px", border: "2px solid gray", borderRadius: "15px", alignItems: "center", justifyContent: "center"}}>
+                    <h3>Добавить фильм</h3>
+                    <div className='container-admin-panel'>
+                        <div className='form-add-movie'>
+                            <input
+                                    className="input-add-movie"
+                                    placeholder="Название..."
+                                    value={name}
+                                    onChange={e => setName(e.target.value)}
+                            />
+                            <input
+                                    className="input-add-movie"
+                                    placeholder="Рейтинг"
+                                    value={rating}
+                                    onChange={e => setRating(e.target.value)}
+                            />
+                            <input
+                                    className="input-add-movie"
+                                    placeholder="Путь к изображению.."
+                                    value={image}
+                                    onChange={e => setImage(e.target.value)}
+                            />
+                            <input
+                                    className="input-add-movie"
+                                    placeholder="Описание..."
+                                    value={description}
+                                    onChange={e => setDescription(e.target.value)}
+                            />
+                            <input
+                                    className="input-add-movie"
+                                    placeholder="Жанр..."
+                                    value={typeId}
+                                    onChange={e => setTypeId(e.target.value)}
+                            />
+                            <input
+                                    className="input-add-movie"
+                                    placeholder="Студия..."
+                                    value={brandId}
+                                    onChange={e => setBrandId(e.target.value)}
+                            />
+                            <button
+                                        className='btn-admin-panel'
+                                        onClick={addMovie}
+                                    >
+                                        {'Добавить фильм'}
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div style={{display:"flex", flexDirection:"column", marginBottom:"20px", border: "2px solid gray", borderRadius: "15px", alignItems: "center", justifyContent: "center"}}>
+                    <h3>Добавить Жанр</h3>
+                    <div className='container-admin-panel'>
+                        <div className='form-add-movie'>
+                            <input
+                                    className="input-add-movie"
+                                    placeholder="Жанр..."
+                                    value={typeName}
+                                    onChange={e => setTypeName(e.target.value)}
+                            />
+                            <button
+                                        className='btn-admin-panel'
+                                        onClick={addType}
+                                    >
+                                        {'Добавить жанр'}
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div style={{display:"flex", flexDirection:"column", border: "2px solid gray", marginBottom:"20px",borderRadius: "15px", alignItems: "center", justifyContent: "center"}}>
+                    <h3>Добавить Студию</h3>
+                    <div className='container-admin-panel'>
+                        <div className='form-add-movie'>
+                            <input
+                                    className="input-add-movie"
+                                    placeholder="Название студии.."
+                                    value={brandName}
+                                    onChange={e => setBrandName(e.target.value)}
+                            />
+                            <button
+                                        className='btn-admin-panel'
+                                        onClick={addBrand}
+                                    >
+                                        {'Добавить студию'}
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
